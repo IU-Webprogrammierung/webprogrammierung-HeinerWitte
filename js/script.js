02623b1745f3593272b464f3a55e86f490982639
HeinerWitte <heiner.witte@iu-study.org>
2026-06-03 15:57:36 +0200

    fix: :bug: DOMContentLoader-Listener in js ergänzen

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-loadComponent("header", "components/header.html");
-loadComponent("footer", "components/footer.html");
-
-
 async function loadComponent(selector, path) {
-    
+    const element = document.querySelector(selector);
+    if (!element) return;
     const response = await fetch(path);
     const html = await response.text();
-    const element = document.querySelector(selector);
     element.innerHTML = html;
-
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadComponent("header", "components/header.html");
+    loadComponent("footer", "components/footer.html");
+});
