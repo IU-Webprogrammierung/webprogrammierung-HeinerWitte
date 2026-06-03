64e03761d89695415b8af17f010902a40818da2c
HeinerWitte <heiner.witte@iu-study.org>
2026-06-03 14:57:11 +0200

    refactor: :art: header mit js in components auslagern

---
+++ js/script.js
@@ -0,0 +1,12 @@
+loadComponent("header", "components/header.html");
+loadComponent("footer", "components/footer.html");
+
+
+async function loadComponent(selector, path) {
+    
+    const response = await fetch(path);
+    const html = await response.text();
+    const element = document.querySelector(selector);
+    element.innerHTML = html;
+
+}
